40bbcf0765b17b7320824264991c639c32181314
FavourB <shoreezbawa@gmail.com>
2020-11-08 15:24:01 +0100

    fixing bugs

--- src/components/landingPage/SearchPane2.jsx
+++ src/components/landingPage/SearchPane2.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-// import axios from 'axios';
+import React, { useState } from 'react';
 import moment from 'moment';
 import { withRouter, /*Redirect, NavLink */} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import { Formik, useFormikContext } from 'formik'; 
 import * as yup from "yup";
 import MenuItem from '@material-ui/core/MenuItem';
@@ -12,12 +12,9 @@ import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import MyLocationIcon from '@material-ui/icons/MyLocation';
 import Geocode from "react-geocode";
-// import Snackbar from '@material-ui/core/Snackbar';
-// import Alert from '@material-ui/lab/Alert';
-// import CloseIcon from '@material-ui/icons/Close';
 
 const today = new Date();
-const styles = theme => ({	
+const useStyles = makeStyles((theme) => ({	
 	errortext:{		
 			color: "#ff0000",
 			lineHeight: 1,
@@ -26,7 +23,7 @@ const styles = theme => ({
 			paddingLeft:theme.spacing(1),
 			paddingTop:theme.spacing(1)		
 	},	
-   });
+   }));
    const CssTextField = withStyles({
 	root: {	 
 	  '& .MuiInputBase-root': {
@@ -36,117 +33,7 @@ const styles = theme => ({
 	},
   })(TextField);
 
-   class SearchPane extends Component {
-	state = {
-		latitude: 38.736946,
-		longitude: -9.142685,
-		location: "",
-		noOfChildren: 0,
-		noOfAdult: 0,
-		checkInDateTime: "",
-		checkOutDateTime: "",
-		size: 3,
-		data: null,
-		isDataNotComplete: true,
-		isNotError: false,
-		startDateNotSet: false,
-		endDateNotSet: false,
-		locationNotSet: false,
-	}
-
-	handleGetLocation = () => {
-		window.navigator.geolocation.getCurrentPosition(
-         success => this.setState({ latitude: success.coords.latitude, longitude: success.coords.longitude }, () => {
-			console.log('Latitide: ', this.state.latitude);
-			console.log('Longitude: ', this.state.longitude);	
-
-			Geocode.setApiKey("AIzaSyC00L07VTlenchjOPLk1lY0hVAK-Rih0go");
-			Geocode.fromLatLng(this.state.latitude, this.state.longitude).then(
-				response => {
-				const address = response.results[0].formatted_address;
-				console.log(address);
-				this.setState({location : address})
-				},
-				error => {
-				console.error(error);
-				}
-			);
-		 })
-         // error=> console.log(error);
-     	);
-		
-	}
-	// displayDataNotCompleteAlert=()=>{
-	// 	this.setState({this.})
-	// }
-	handleGetLatLong = () => {}
-	// handleClose = (event, reason) => {
- //    if (reason === 'clickaway') {
- //      return;
- //    }
-
- //    this.setState({isDataNotComplete: false});
- //  	}
- 	handleSelectChange =(e)=> {
-		this.setState({ [e.target.name]: e.target.value })
-	}
-	handleChange = (e) => {
-		this.setState({ [e.target.id]: e.target.value })
-	}
- 	handleSelectChange =(e)=> {
-		this.setState({ [e.target.name]: e.target.value })
-	}
-	handleChange = (e) => {
-		this.setState({ [e.target.id]: e.target.value })
-	}
-	isFieldComplete=()=>{
-		if(this.state.checkInDateTime === ""){
-			this.setState({ startDateNotSet: true });
-		}
-		if(this.state.checkOutDateTime === ""){
-			this.setState({ endDateNotSet: true });
-		}
-		if(this.state.location === "" || (this.state.latitude === "" && this.state.longitude === "")){
-			this.setState({ locationNotSet: true });
-		}
-		// if(this.state.startDateNotSet === false && this.state.endDateNotSet === false && this.state.locationNotSet === false){
-		// 	return true;
-		// }
-	}
-	handleSubmit = async (e) => {
-		//e.preventDefault();
-		this.isFieldComplete();
-		// console.log(this.state.noOfAdult, this.state.noOfChildren)
-
-		// console.log(this.state.startDateNotSet)
-		// console.log(this.state.endDateNotSet)
-		// console.log(this.state.locationNotSet)
-		// console.log(this.state)
-
-		this.setState({
-			data: {
-				location: this.state.location,
-				latitude: this.state.latitude,
-				longitude: this.state.longitude,
-				start: moment(this.state.checkInDateTime).format("YYYY-MM-DD"),
-				end: moment(this.state.checkOutDateTime).format("YYYY-MM-DD"),
-				size: this.state.size,
-			}
-		}, ()=>{
-			if(this.state.longitude === "" || this.state.latitude === ""){
-				this.setState({ isDataNotComplete: true });
-				console.log(this.state.isDataNotComplete);
-			} else {
-				this.props.history.push({ 
-				 pathname: '/hotels',
-				 state: { searchData: this.state.data, guestCount: this.state.noOfAdult + this.state.noOfChildren }
-				});
-				this.setState({ isDataNotComplete: false, isNotError: true });
-			}
-		})
-	}
-	render() {
-		const { classes } = this.props;
+  
 		
 		// const { startDateNotSet, endDateNotSet, locationNotSet } = this.state;
 	   // if (this.state.isNotError === true) {
@@ -188,7 +75,7 @@ const styles = theme => ({
 			value: "4+",  label:  "4+"
 			}
 			];
-		const today = new Date();
+		
 	   	const validationSchema = yup.object().shape({
 		noOfAdults:  yup.string().required("Select No of Adults"),
 		noOfChildren:  yup.string().required("Select No of Children"), 
@@ -198,8 +85,88 @@ const styles = theme => ({
 		location: yup.string().required("Required"),
 	  });
 
-		return (
-			<div className="container-fluid searchPaneBackground">
+
+const SearchPane = () => {
+    const classes = useStyles();
+   // const {values} = useFormikContext();
+    const [search, setSearch] = useState( {
+		latitude: 38.736946,
+		longitude: -9.142685,
+		location: "",
+		noOfChildren:"",
+		noOfAdults: "",
+		checkInDateTime: "",
+		checkOutDateTime: "",
+		size: 3,
+		data: null,
+		isDataNotComplete: true,
+		isNotError: false,
+		startDateNotSet: false,
+		endDateNotSet: false,
+		locationNotSet: false,
+    });
+    
+   const handleGetLocation = () => {
+		window.navigator.geolocation.getCurrentPosition(
+         success => this.setState({ latitude: success.coords.latitude, longitude: success.coords.longitude }, () => {
+			console.log('Latitide: ', this.state.latitude);
+			console.log('Longitude: ', this.state.longitude);	
+
+			Geocode.setApiKey("AIzaSyC00L07VTlenchjOPLk1lY0hVAK-Rih0go");
+			Geocode.fromLatLng(this.state.latitude, this.state.longitude).then(
+				response => {
+				const address = response.results[0].formatted_address;
+				console.log(address);
+				this.setState({location : address})
+				},
+				error => {
+				console.error(error);
+				}
+			);
+		 })
+         // error=> console.log(error);
+     	);
+		
+	}
+	// displayDataNotCompleteAlert=()=>{
+	// 	this.setState({this.})
+	// }
+	const handleGetLatLong = () => {}
+	// handleClose = (event, reason) => {
+ //    if (reason === 'clickaway') {
+ //      return;
+ //    }
+
+ //    this.setState({isDataNotComplete: false});
+ //  	}
+ 	
+	
+	//}
+	const handleSubmit = async (e) => {
+		//e.preventDefault();
+		// this.isFieldComplete();
+		// console.log(this.state.noOfAdult, this.state.noOfChildren)
+
+		// console.log(this.state.startDateNotSet)
+		// console.log(this.state.endDateNotSet)
+		// console.log(this.state.locationNotSet)
+		// console.log(this.state)
+	this.props.history.push({ 
+				 pathname: '/hotels',
+				 search: { searchData: search.data, guestCount:search.noOfAdult +search.noOfChildren }
+		// setSearch({
+			
+		
+		
+			
+		// 		});
+		// 		setSearch({ isDataNotComplete: false, isNotError: true });
+			
+		// })
+	})}
+
+    return ( 
+        <div className="container-fluid searchPaneBackground">
 				{/*<Snackbar
 					open={this.state.isDataNotComplete}
 					autoHideDuration={3000}
@@ -217,21 +184,9 @@ const styles = theme => ({
 			      </Alert>
 				</Snackbar>*/}
 				<Formik
-					initialValues={{
-						noOfChildren: "",
-						noOfAdults: "",
-						checkInDateTime: "",
-						checkOutDateTime: "",
-						location: "",
-					}} 
+					initialValues={search}
 					validationSchema={validationSchema}
-					onSubmit= {()=>this.handleSubmit()}
-						// this.props.history.push({ 
-						//  pathname: '/hotels'})
-						//  state: { searchData: this.state.data, guestCount: this.state.noOfAdult + this.state.noOfChildren }
-						// })
-						
-					//}
+					onSubmit= { handleSubmit}
 				>
 					 {({
 						values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting
@@ -242,7 +197,7 @@ const styles = theme => ({
 			        	<div className="mb-2 col-sm-12 col-md-4 col-lg-3 col-xl-3">
 					      <div className="d-flex align-items-center justify-content-between">
 				        		<div className="pr-1">
-					        		<IconButton className="text-white" onClick={this.handleGetLocation}>
+					        		<IconButton className="text-white" onClick={handleGetLocation}>
 					        			<MyLocationIcon fontSize="large" />
 					        		</IconButton>
 				        		</div>
@@ -366,8 +321,7 @@ const styles = theme => ({
 				)}
 				</Formik>
 		   </div>
-		);
-	}
+     );
 }
-
-export default withRouter((withStyles(styles)(SearchPane)));
+ 
+export default withRouter(SearchPane);
