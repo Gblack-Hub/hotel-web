d49a30464a8be27903814dddf5893f3033ba632c
Gblack-Hub <oladiranwale@gmail.com>
2020-11-08 16:10:46 +0100

    search pane converted to functional component

--- src/components/landingPage/SearchPane.jsx
+++ src/components/landingPage/SearchPane.jsx
@@ -37,50 +37,12 @@ const styles = theme => ({
 	},
   })(TextField);
 
-   class SearchPane extends Component {
-	state = {
-		latitude: 38.736946,
-		longitude: -9.142685,
-		location: "",
-		noOfChildren: 0,
-		noOfAdult: 0,
-		checkInDateTime: "",
-		checkOutDateTime: "",
-		size: 3,
-		data: null,
-		isDataNotComplete: true,
-		isNotError: false,
-		startDateNotSet: false,
-		endDateNotSet: false,
-		locationNotSet: false,
-	}
-
-	handleGetLocation = () => {
-		window.navigator.geolocation.getCurrentPosition(
-         success => this.setState({ latitude: success.coords.latitude, longitude: success.coords.longitude }, () => {
-			console.log('Latitide: ', this.state.latitude);
-			console.log('Longitude: ', this.state.longitude);	
-
-			Geocode.setApiKey("AIzaSyC00L07VTlenchjOPLk1lY0hVAK-Rih0go");
-			Geocode.fromLatLng(this.state.latitude, this.state.longitude).then(
-				response => {
-				const address = response.results[0].formatted_address;
-				console.log(address);
-				this.setState({location : address})
-				},
-				error => {
-				console.error(error);
-				}
-			);
-		 })
-         // error=> console.log(error);
-     	);
-		
-	}
 	// displayDataNotCompleteAlert=()=>{
 	// 	this.setState({this.})
 	// }
-	handleGetLatLong = () => {}
+
+	// const handleGetLatLong = () => {}
+
 	// handleClose = (event, reason) => {
  //    if (reason === 'clickaway') {
  //      return;
@@ -88,91 +50,95 @@ const styles = theme => ({
 
  //    this.setState({isDataNotComplete: false});
  //  	}
- 	handleSelectChange =(e)=> {
-		this.setState({ [e.target.name]: e.target.value })
-	}
-	handleChange = (e) => {
-		this.setState({ [e.target.id]: e.target.value })
-	}
- 	handleSelectChange =(e)=> {
-		this.setState({ [e.target.name]: e.target.value })
-	}
-	handleChange = (e) => {
-		this.setState({ [e.target.id]: e.target.value })
-	}
-	isFieldComplete=()=>{
-		if(this.state.checkInDateTime === ""){
-			this.setState({ startDateNotSet: true });
-		}
-		if(this.state.checkOutDateTime === ""){
-			this.setState({ endDateNotSet: true });
-		}
-		if(this.state.location === "" || (this.state.latitude === "" && this.state.longitude === "")){
-			this.setState({ locationNotSet: true });
-		}
-		// if(this.state.startDateNotSet === false && this.state.endDateNotSet === false && this.state.locationNotSet === false){
-		// 	return true;
-		// }
-	}
-	handleSubmit = async (e) => {
-		//e.preventDefault();
-		this.isFieldComplete();
-		// console.log(this.state.noOfAdult, this.state.noOfChildren)
 
-		// console.log(this.state.startDateNotSet)
-		// console.log(this.state.endDateNotSet)
-		// console.log(this.state.locationNotSet)
-		// console.log(this.state)
-
-		this.setState({
-			data: {
-				location: this.state.location,
-				latitude: this.state.latitude,
-				longitude: this.state.longitude,
-				start: moment(this.state.checkInDateTime).format("YYYY-MM-DD"),
-				end: moment(this.state.checkOutDateTime).format("YYYY-MM-DD"),
-				size: this.state.size,
-			}
-		}, ()=>{
-			if(this.state.longitude === "" || this.state.latitude === ""){
-				this.setState({ isDataNotComplete: true });
-				console.log(this.state.isDataNotComplete);
-			} else {
-				this.props.history.push({ 
-				 pathname: '/hotels',
-				 state: { searchData: this.state.data, guestCount: this.state.noOfAdult + this.state.noOfChildren }
-				});
-				this.setState({ isDataNotComplete: false, isNotError: true });
-			}
-		})
+	// handleChange = (e) => {
+	// 	this.setState({ [e.target.id]: e.target.value })
+	// }
+ 	// handleSelectChange =(e)=> {
+	// 	this.setState({ [e.target.name]: e.target.value })
+	// }
+	const handleBlur=()=>{
+		console.log('here')
 	}
-	render() {
-		const { classes } = this.props;
-		
-		// const { startDateNotSet, endDateNotSet, locationNotSet } = this.state;
-	   // if (this.state.isNotError === true) {
-	   //    return <Redirect 
-	   //    			to={{
-	   //    				pathname: '/hotels',
-	   //    				state: { searchData: this.state.data }
-	   //    			}}
-	   //    		/>
-	   // }
-	   const noOfAdults = [
-		{
-		  value: "1", label:  "1"
-		},
-		{
-		  value: "2", label:  "2"
-		},
-		{
-		  value: "3", label:  "3"
-		},
-		{
-		value: "4+",  label:  "4+"
+
+	
+	const SearchPane =()=> {
+		// const { classes } = this.props;
+
+		const [latitude, setLatitude] = React.useState(38.736946);
+		const [longitude, setLongitude] = React.useState(-9.142685);
+		const [location, setLocation] = React.useState("");
+		const [noOfChildren, setNoOfChildren] = React.useState(0);
+		const [noOfAdult, setNoOfAdult] = React.useState(0);
+		const [checkInDateTime, setCheckInDateTime] = React.useState("");
+		const [checkOutDateTime, setCheckOutDateTime] = React.useState("");
+		const [data, setData] = React.useState(null);
+		const [isDataNotComplete, setIsDataNotComplete] = React.useState(true);
+		const [isNotError, setIsNotError] = React.useState(false);
+
+		const handleGetLocation = () => {
+			window.navigator.geolocation.getCurrentPosition(
+			 success => this.setState({ latitude: success.coords.latitude, longitude: success.coords.longitude }, () => {
+				console.log('Latitide: ', latitude);
+				console.log('Longitude: ', longitude);	
+	
+				Geocode.setApiKey("AIzaSyC00L07VTlenchjOPLk1lY0hVAK-Rih0go");
+				Geocode.fromLatLng(this.state.latitude, this.state.longitude).then(
+					response => {
+					const address = response.results[0].formatted_address;
+					console.log(address);
+					setLocation(address)
+					// this.setState({location : address})
+					},
+					error => {
+						console.error(error);
+					}
+				);
+			 })
+			 // error=> console.log(error);
+			 );
+			
 		}
+
+		const handleSubmit = async (e) => {
+			this.setState({
+				data: {
+					location: this.state.location,
+					latitude: this.state.latitude,
+					longitude: this.state.longitude,
+					start: moment(this.state.checkInDateTime).format("YYYY-MM-DD"),
+					end: moment(this.state.checkOutDateTime).format("YYYY-MM-DD"),
+					size: this.state.size,
+				}
+			}, ()=>{
+				if(this.state.longitude === "" || this.state.latitude === ""){
+					this.setState({ isDataNotComplete: true });
+					console.log(this.state.isDataNotComplete);
+				} else {
+					this.props.history.push({ 
+					 pathname: '/hotels',
+					 state: { searchData: this.state.data, guestCount: this.state.noOfAdult + this.state.noOfChildren }
+					});
+					this.setState({ isDataNotComplete: false, isNotError: true });
+				}
+			})
+		}
+
+	   	const adults = [
+			{
+			value: "1", label:  "1"
+			},
+			{
+			value: "2", label:  "2"
+			},
+			{
+			value: "3", label:  "3"
+			},
+			{
+			value: "4+",  label:  "4+"
+			}
 		];
-		const noOfChildren = [
+		const children = [
 			{
 				value: "0", label:  "0"
 			},
@@ -188,36 +154,20 @@ const styles = theme => ({
 			{
 			value: "4+",  label:  "4+"
 			}
-			];
+		];
 		const today = new Date();
-	   	const validationSchema = yup.object().shape({
-		noOfAdults:  yup.string().required("Select No of Adults"),
-		noOfChildren:  yup.string().required("Select No of Children"), 
-		checkInDateTime: yup.date().required("Required").test('start', () => {  return yup.date().min(today) }),
-		checkOutDateTime: yup.date().min(yup.ref('checkInDateTime'),
-        "Check out date can't be before Check in date").required("Required"),
-		location: yup.string().required("Required"),
-	  });
+	//    	const validationSchema = yup.object().shape({
+	// 	noOfAdults:  yup.string().required("Select No of Adults"),
+	// 	noOfChildren:  yup.string().required("Select No of Children"), 
+	// 	checkInDateTime: yup.date().required("Required").test('start', () => {  return yup.date().min(today) }),
+	// 	checkOutDateTime: yup.date().min(yup.ref('checkInDateTime'),
+    //     "Check out date can't be before Check in date").required("Required"),
+	// 	location: yup.string().required("Required"),
+	//   });
 
 		return (
 			<div className="container-fluid searchPaneBackground">
-				{/*<Snackbar
-					open={this.state.isDataNotComplete}
-					autoHideDuration={3000}
-					onClose={this.handleClose}
-					action={
-						<React.Fragment>
-			            <IconButton size="small" aria-label="close" color="inherit" onClick={this.handleClose}>
-			              <CloseIcon fontSize="small" />
-			            </IconButton>
-			         </React.Fragment>
-		        	}
-					>
-					<Alert variant="filled" severity="error">
-			        Please enter a location
-			      </Alert>
-				</Snackbar>*/}
-				<Formik
+				{/* <Formik
 					initialValues={{
 						noOfChildren: "",
 						noOfAdults: "",
@@ -232,30 +182,30 @@ const styles = theme => ({
 						//  state: { searchData: this.state.data, guestCount: this.state.noOfAdult + this.state.noOfChildren }
 						// })
 						
-					//}
+						//}
 				>
 					 {({
 						values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting
-					}) =>(
+					}) =>( */}
 			
 					<form autoComplete="off" onSubmit={handleSubmit}>
 					<div className="py-2 row justify-content-center align-items-center">
 			        	<div className="mb-2 col-sm-12 col-md-4 col-lg-3 col-xl-3">
 					      <div className="d-flex align-items-center justify-content-between">
 				        		<div className="pr-1">
-					        		<IconButton className="text-white" onClick={this.handleGetLocation}>
+					        		<IconButton className="text-white" onClick={handleGetLocation}>
 					        			<MyLocationIcon fontSize="large" />
 					        		</IconButton>
 				        		</div>
 					        	<CssTextField 
-									FormHelperTextProps={{className:classes.errortext}}
+									// FormHelperTextProps={{className:classes.errortext}}
 									id="location"
 									size="small"
 									name="location"
-									value={values.location}
-									helperText={touched.location ? errors.location : ""}
-									error={touched.location && Boolean(errors.location)}
-									onChange={handleChange}
+									// value={values.location}
+									// helperText={touched.location ? errors.location : ""}
+									// error={touched.location && Boolean(errors.location)}
+									onChange={e => setLocation(e.target.value)}
 									onBlur={handleBlur}
 									fullWidth
 									label="Enter Location"
@@ -265,19 +215,19 @@ const styles = theme => ({
 					   	</div>
 			        	<div className="mb-2 col-sm-12 col-md-4 col-lg-3 col-xl-2">
 							<CssTextField  
-								className= {classes.input}
-								FormHelperTextProps={{className:classes.errortext}}
+								// className= {classes.input}
+								// FormHelperTextProps={{className:classes.errortext}}
 				         		variant="filled"
 								id="checkInDateTime"
 								name="checkInDateTime"
 								label="Check in Date"
 								size="small"
-								helperText={touched.checkInDateTime ? errors.checkInDateTime : ""}
-								error={touched.checkInDateTime && Boolean(errors.checkInDateTime)}
+								// helperText={touched.checkInDateTime ? errors.checkInDateTime : ""}
+								// error={touched.checkInDateTime && Boolean(errors.checkInDateTime)}
 								InputProps={{inputProps: { min: moment(today).add(1, 'days').format("YYYY-MM-DD") } }}
 							    fullWidth
 							   	onBlur={handleBlur}
-							    onChange={handleChange}
+								onChange={e => setCheckInDateTime(e.target.value)}
 							    type="date"
 							    InputLabelProps={{
 								  shrink: true,
@@ -286,16 +236,16 @@ const styles = theme => ({
 			        	</div>
 			        	<div className="mb-2 col-sm-12 col-md-4 col-lg-3 col-xl-2">
 				         <CssTextField 
-						 	FormHelperTextProps={{className:classes.errortext}}
+						 	// FormHelperTextProps={{className:classes.errortext}}
 				         	variant="filled"
 							id="checkOutDateTime"
 							name="checkOutDateTime"
 							label="Check out Date"
-							helperText={touched.checkOutDateTime ? errors.checkOutDateTime : ""}
-							error={touched.checkOutDateTime && Boolean(errors.checkOutDateTime)} 
+							// helperText={touched.checkOutDateTime ? errors.checkOutDateTime : ""}
+							// error={touched.checkOutDateTime && Boolean(errors.checkOutDateTime)} 
 							size="small"
 							fullWidth
-							onChange={handleChange}
+							onChange={e => setCheckOutDateTime(e.target.value)}
 							onBlur={handleBlur}
 							type="date"
 							className="rounded bg-light"
@@ -308,27 +258,26 @@ const styles = theme => ({
 				        	{/* <div className="mr-2 flex-fill mb-2 col-sm-12 col-md-4 col-lg-3 col-xl-3 "> */}
 								<FormControl size="small" fullWidth variant="filled" className="rounded bg-light">
 										<CssTextField 
-											FormHelperTextProps={{className:classes.errortext}}
+											// FormHelperTextProps={{className:classes.errortext}}
 											select
 											variant="filled"
 											size="small" 
 											label="No of Adults"										
 											id="noOfAdults"
 											name="noOfAdults"
-											value={values.noOfAdults}
-											onChange={handleChange}
+											// value={values.noOfAdults}
+											onChange={e => setNoOfAdult(e.target.value)}
 											onBlur={handleBlur}
-											helperText={touched.noOfAdults ? errors.noOfAdults : ""}
-											error={touched.noOfAdults && Boolean(errors.noOfAdults)} 
+											// helperText={touched.noOfAdults ? errors.noOfAdults : ""}
+											// error={touched.noOfAdults && Boolean(errors.noOfAdults)} 
 											
 										>
-											{noOfAdults.map(option => (
+											{adults.map(option => (
 												<MenuItem key={option.value} value={option.value}>
 												{option.label}
 												</MenuItem>
 											))}
 									</CssTextField >
-								
 								</FormControl>
 				        	</div>
 				        	<div className=" mb-2 col-sm-12 col-md-4 col-lg-3 col-xl-2 ">
@@ -336,20 +285,19 @@ const styles = theme => ({
 									
 									<CssTextField 
 									select
-										FormHelperTextProps={{className:classes.errortext}}
+										// FormHelperTextProps={{className:classes.errortext}}
 										variant="filled"
 										size="small" 
 										label="No of Children"
 										id="noOfChildren"
 										name="noOfChildren"
-										value={values.noOfChildren}
-										onChange={handleChange}
+										// value={values.noOfChildren}
+										onChange={e => setNoOfChildren(e.target.value)}
 										onBlur={handleBlur}
-										helperText={touched.noOfChildren ? errors.noOfChildren : ""}
-										error={touched.noOfChildren && Boolean(errors.noOfChildren)} 		
-												
+										// helperText={touched.noOfChildren ? errors.noOfChildren : ""}
+										// error={touched.noOfChildren && Boolean(errors.noOfChildren)}
 									>
-										{noOfChildren.map(option => (
+										{children.map(option => (
 											<MenuItem key={option.value} value={option.value}>
 											{option.label}
 											</MenuItem>			
@@ -359,16 +307,14 @@ const styles = theme => ({
 				        	</div>
 				      	
 			        	<div className="mb-2 text-center col-sm-12 col-md-4 col-lg-2 col-xl-1">
-							<Button variant="contained" type="submit" size="large" color="secondary" fullWidth className="text-white" 
-							disabled={isSubmitting}>Search</Button>
+							<Button variant="contained" type="submit" size="large" color="secondary" fullWidth className="text-white">Search</Button>
 			       		</div>
 			      	</div>
 		      		</form>
-				)}
-				</Formik>
+				{/* )}
+				</Formik> */}
 		   </div>
 		);
 	}
-}
 
 export default withRouter((withStyles(styles)(SearchPane)));
